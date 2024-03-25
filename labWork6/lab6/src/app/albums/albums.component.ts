5b80c0e71fdfbb3080afdb7f3eb2f1d98acc8fb5
Abdumalik1104 <123067373+Abdumalik1104@users.noreply.github.com>
2024-03-25 09:43:18 +0600

    lab7

--- labWork6/lab6/src/app/albums/albums.component.ts
+++ labWork6/lab6/src/app/albums/albums.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AlbumsService } from '../albums.service';
 import { Album } from '../models';
 
 @Component({
   selector: 'app-albums',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './albums.component.html',
   styleUrl: './albums.component.css'
 })
 export class AlbumsComponent implements OnInit, OnDestroy{
   albums!: Album[];
+  newAlbum: Album;
   loaded: boolean = false;
 
   constructor(private albumService: AlbumsService){
+    this.newAlbum = {
+      userId: 1,
+      id: 0,
+      title: ''
+    }
   }
 
   ngOnInit() {
@@ -24,6 +31,22 @@ export class AlbumsComponent implements OnInit, OnDestroy{
       this.albums = albums;
       this.loaded = true;
     } )
+  }
+  counter: number = 0;
+
+  addAlbum() {
+    let maxId = this.albums[this.albums.length - 1].id;
+    this.counter = this.counter + 1;
+    this.newAlbum.id = this.counter;
+    console.log(this.newAlbum);
+    this.albumService.createAlbum(this.newAlbum).subscribe((album) => {
+      this.albums.push(album);
+      this.newAlbum = {
+        userId: 1,
+        id: 0,
+        title: ''
+      }
+    })
   }
 
   deleteAlbum(id: number) {
